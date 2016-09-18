9f962421b9195e05d1295f16f7bd5a51aeff659e
Radu <miron.radu.constantin@gmail.com>
2016-09-18 13:17:20 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $('nav a').click(function(event) {
+    $(this).css("text-decoration","none");
+    $(this).css("color","white");
+    event.preventDefault();
+    var anchorTo = $(this);
+    $('body, html').animate({
+      scrollTop: $(anchorTo.attr('href')).offset().top
+    }, 1000);
+   });
+ 
+
+});
