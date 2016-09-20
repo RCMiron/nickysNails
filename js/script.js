f4ffc3cc7ca0b2044cd5ffc45ac1fc83f679d692
Radu <miron.radu.constantin@gmail.com>
2016-09-20 10:29:33 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,6 +9,18 @@ $(document).ready(function() {
       scrollTop: $(anchorTo.attr('href')).offset().top
     }, 1000);
    });
- 
+   if($('nav').css('height','800px'))
+   {
+     $('nav li').click(function(){
+       $('nav').css('margin','5px').css('height','50px').css('width','50px').css('overflow','hidden').css('background-image','url(../assets/figerprint.png)').css('background-size','contain').css('background-repeat','no-repeat');
+     })
+   }
+         margin:5px;
+        height:50px;
+        width:50px;
+        overflow: hidden;
+        background-image: url(../assets/figerprint.png);
+        background-size: contain;
+        background-repeat: no-repeat;
 
 });
