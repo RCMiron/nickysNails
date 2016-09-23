bd0e615c2a759692d9fc627c29a265f6172c0bf6
Radu <miron.radu.constantin@gmail.com>
2016-09-23 12:57:21 +0300

    position the arrowUp and add functionality

--- js/script.js
+++ js/script.js
@@ -1,76 +1,77 @@
-$(window).load(function() {
+$(document).ready(function() {
 
-$('nav a').click(function(event) {
-    $(this).css("text-decoration","none");
-    $(this).css("color","white");
-    event.preventDefault();
-    var anchorTo = $(this);
-    $('body, html').animate({
-      scrollTop: $(anchorTo.attr('href')).offset().top
-    }, 1000);
-   });
-$(window).scroll(function(){
-     var where = $(this).scrollTop();
-     $('nav li').css('transition','border-bottom 0.5s ease');
-     if (window.matchMedia("(min-width: 500px)").matches){
-             $('nav li').css('border-bottom','none');
-             if(where < $(this).height()*0.8){
-       -       $('.home').css('text-shadow','2px 2px 5px #EC008C');
-       -       $('.about, .gallery, .services, .contact').css('text-shadow','none');
-             }
-             else if(where < $(this).height()*1.8){
-       -       $('.about').css('text-shadow','2px 2px 5px #EC008C');
-       -       $('.home, .gallery, .services, .contact').css('text-shadow','none');
-             }
-             else if(where < $(this).height()*2.8){
-       -       $('.gallery').css('text-shadow','2px 2px 5px #EC008C');
-       -       $('.about, .home, .services, .contact').css('text-shadow','none');
-             }
-             else if(where < $(this).height()*3.8){
-       -       $('.services').css('text-shadow','2px 2px 5px #EC008C');
-       -       $('.about, .gallery, .home, .contact').css('text-shadow','none');
-             }
-             else if(where < $(this).height()*4.8){
-       -       $('.contact').css('text-shadow','2px 2px 5px #EC008C');
-       -       $('.about, .gallery, .services, .home').css('text-shadow','none');
-             }
-      }
-     else {
-             if(where < $(this).height()*0.8){
-       -       $('.home').css('border-bottom','3px solid rgba(255,0,100,1)').css('text-shadow','2px 2px 5px #EC008C');
-       -       $('.about, .gallery, .services, .contact').css('border-bottom','3px solid rgba(255,255,255,0.5)').css('text-shadow','none');
-             }
-             else if(where < $(this).height()*1.8){
-       -       $('.about').css('border-bottom','3px solid rgba(255,0,100,1)').css('text-shadow','2px 2px 5px #EC008C');
-       -       $('.home, .gallery, .services, .contact').css('border-bottom','3px solid rgba(255,255,255,0.5)').css('text-shadow','none');
-             }
-             else if(where < $(this).height()*2.8){
-       -       $('.gallery').css('border-bottom','3px solid rgba(255,0,100,1)').css('text-shadow','2px 2px 5px #EC008C');
-       -       $('.about, .home, .services, .contact').css('border-bottom','3px solid rgba(255,255,255,0.5)').css('text-shadow','none');
-      
-             }
-             else if(where < $(this).height()*3.8){
-       -       $('.services').css('border-bottom','3px solid rgba(255,0,100,1)').css('text-shadow','2px 2px 5px #EC008C');
-       -       $('.about, .gallery, .home, .contact').css('border-bottom','3px solid rgba(255,255,255,0.5)').css('text-shadow','none');
-             }
-             else if(where < $(this).height()*4.8){
-       -       $('.contact').css('border-bottom','3px solid rgba(255,0,100,1)').css('text-shadow','2px 2px 5px #EC008C');
-       -       $('.about, .gallery, .services, .home').css('border-bottom','3px solid rgba(255,255,255,0.5)').css('text-shadow','none');
-             }
-        }
-
-	});
-	
-if ($(window).scrollTop() > 100) {
-	$('.scrollToTop').fadeIn();
-	} 
-else {
-	$('.scrollToTop').fadeOut();
-	}
-$('.scrollToTop').click(function(){
-$('html, body').animate({scrollTop : 0},800);
-		 
+  $('nav a').click(function(event) {
+      $(this).css("text-decoration","none");
+      $(this).css("color","white");
+      event.preventDefault();
+      var anchorTo = $(this);
+      $('body, html').animate({
+        scrollTop: $(anchorTo.attr('href')).offset().top
+      }, 1000);
      });
-	
-	
+  $(window).scroll(function(){
+    var where = $(this).scrollTop();
+    $('nav li').css('transition','border-bottom 0.5s ease');
+    if (window.matchMedia("(min-width: 500px)").matches){
+        $('nav li').css('border-bottom','none');
+        if(where < $(this).height()*0.8){
+          $('.home').css('text-shadow','2px 2px 5px #EC008C');
+          $('.about, .gallery, .services, .contact').css('text-shadow','none');
+        }
+        else if(where < $(this).height()*1.8){
+          $('.about').css('text-shadow','2px 2px 5px #EC008C');
+          $('.home, .gallery, .services, .contact').css('text-shadow','none');
+        }
+        else if(where < $(this).height()*2.8){
+          $('.gallery').css('text-shadow','2px 2px 5px #EC008C');
+          $('.about, .home, .services, .contact').css('text-shadow','none');
+        }
+        else if(where < $(this).height()*3.8){
+          $('.services').css('text-shadow','2px 2px 5px #EC008C');
+          $('.about, .gallery, .home, .contact').css('text-shadow','none');
+        }
+        else if(where < $(this).height()*4.8){
+          $('.contact').css('text-shadow','2px 2px 5px #EC008C');
+          $('.about, .gallery, .services, .home').css('text-shadow','none');
+        }
+    }
+    else {
+        if(where < $(this).height()*0.8){
+          $('.home').css('border-bottom','3px solid rgba(255,0,100,1)').css('text-shadow','2px 2px 5px #EC008C');
+          $('.about, .gallery, .services, .contact').css('border-bottom','3px solid rgba(255,255,255,0.5)').css('text-shadow','none');
+        }
+        else if(where < $(this).height()*1.8){
+          $('.about').css('border-bottom','3px solid rgba(255,0,100,1)').css('text-shadow','2px 2px 5px #EC008C');
+          $('.home, .gallery, .services, .contact').css('border-bottom','3px solid rgba(255,255,255,0.5)').css('text-shadow','none');
+        }
+        else if(where < $(this).height()*2.8){
+          $('.gallery').css('border-bottom','3px solid rgba(255,0,100,1)').css('text-shadow','2px 2px 5px #EC008C');
+          $('.about, .home, .services, .contact').css('border-bottom','3px solid rgba(255,255,255,0.5)').css('text-shadow','none');
+        }
+        else if(where < $(this).height()*3.8){
+          $('.services').css('border-bottom','3px solid rgba(255,0,100,1)').css('text-shadow','2px 2px 5px #EC008C');
+          $('.about, .gallery, .home, .contact').css('border-bottom','3px solid rgba(255,255,255,0.5)').css('text-shadow','none');
+        }
+        else if(where < $(this).height()*4.8){
+          $('.contact').css('border-bottom','3px solid rgba(255,0,100,1)').css('text-shadow','2px 2px 5px #EC008C');
+          $('.about, .gallery, .services, .home').css('border-bottom','3px solid rgba(255,255,255,0.5)').css('text-shadow','none');
+        }
+    };
+  });
+  $(window).scroll(function(){
+    var where = $(this).scrollTop();
+    if(where > 100){
+      $('#backToTop').addClass('show');
+    }
+    else{
+      $('#backToTop').removeClass('show');
+    }
+  });
+  $('#backToTop').click(function(){
+    $('html,body').animate({scrollTop: 0}, 700);
+  });
+
+
+
+
 });
